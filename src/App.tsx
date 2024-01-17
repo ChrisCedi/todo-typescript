76115dbc71543bb1ec1a1b0d790de9304ecfb092
ChrisCedi <christian.cedillo.palacios@outlook.com>
2024-01-16 20:30:12 -0600

    filtered todos

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,21 @@
 import { useState } from 'react'
 import { mockTodos } from './mocks/mocks.ts'
 import { Todos } from './components/Todos.tsx'
-import { type TodoId, type Todo as TodoType, type TodoCompleted } from './types'
+import { type TodoId, type Todo as TodoType, type TodoCompleted, type FilterValue } from './types'
+import { TODO_FILTERS } from './consts.ts'
+import { Footer } from './components/Footer.tsx'
 
 const App = (): JSX.Element => {
   const [todos, setTodos] = useState(mockTodos)
+  const [filterSelected, setFilterSelected] = useState<FilterValue>(TODO_FILTERS.ALL)
 
   const handleRemove = ({ id }: TodoId): void => {
     const newTodos = todos.filter(todo => todo.id !== id)
     setTodos(newTodos)
   }
+
+  const activeCount = todos.filter(todo => !todo.completed).length
+  const completedCount = todos.length - activeCount
 
   const handleCompleted = ({ id, completed }: Pick<TodoType, 'id' | 'completed'>): void => {
     const newTodos = todos.map(todo => {
@@ -26,12 +32,28 @@ const App = (): JSX.Element => {
     setTodos(newTodos)
   }
 
+  const filteredTodos = todos.filter(todo => {
+    if (filterSelected === TODO_FILTERS.ACTIVE) return !todo.completed
+    if (filterSelected === TODO_FILTERS.ALL) return todo
+    if (filterSelected === TODO_FILTERS.COMPLETED) return todo.completed
+  })
+
+  const handleFilterChange = (filter: FilterValue): void => {
+    setFilterSelected(filter)
+  }
   return (
    <div className='todoapp'>
      <Todos
      onToggleCompletedTodo={handleCompleted}
      onRemoveTodo={handleRemove}
-     todos={todos}/>
+     todos={filteredTodos}/>
+     <Footer
+      activeCount={activeCount}
+      completedCount={completedCount}
+      filterSelected={filterSelected}
+      handleFilterChange={handleFilterChange}
+      onClearCompleted={() => {}}
+     />
    </div>
   )
 }
